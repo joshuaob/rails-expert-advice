45845d436c672533a7d0fbe6d4268e0c9bc1b582
Joshua Ob <joshuaaob@gmail.com>
2019-12-20 20:35:10 +0000

    Add API routes for questions and posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/users/me', to: 'users#me'
       resources :users
+      resources :questions
+      resources :posts, only: [:index, :create]
     end
   end
 end
